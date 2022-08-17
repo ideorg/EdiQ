7e71ad58a2df4d740fd379675fd3f438af390d72
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-08-17 16:17:15 +0200

    main window is resized relative to screen QScreen, screen->geometry

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow mainWindow;
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    mainWindow.resize(int(screenGeometry.width()*0.8), int(screenGeometry.height()*0.8));
     mainWindow.show();
     return app.exec();
 }
