117e25545ed43806bf4bb5853384fd332ff37071
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-08-18 15:07:32 +0200

    new class UntitledCounter

---
+++ UntitledCounter.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Andrzej Borucki on 2022-08-13
+//
+
+#include "UntitledCounter.h"
+
+int UntitledCounter::getNextId() {
+    int cnt = bitArray->count();
+    int result = cnt;
+    for (int i=1; i<cnt; i++)
+    {
+        if (!bitArray->testBit(i)) {
+            result = i;
+            break;
+        }
+    };
+    assert(bitArray->size()>=result);
+    if (bitArray->size()==result)
+        bitArray->resize(result+RESIZE);
+    bitArray->setBit(result);
+    return result;
+}
+
+bool UntitledCounter::releaseId(int id) {
+    assert(id>=1);
+    assert(bitArray->testBit(id));
+    bitArray->clearBit(id);
+    return true;
+}
+
+UntitledCounter::UntitledCounter() {
+    bitArray = new QBitArray(RESIZE);
+    bitArray->setBit(0); //not used for untitled
+}
---
+++ UntitledCounter.h
@@ -0,0 +1,23 @@
+//
+// Created by Andrzej Borucki on 2022-08-13
+//
+
+#ifndef EDIQ_UNTITLECOUNTER_H
+#define EDIQ_UNTITLECOUNTER_H
+
+
+#include <QBitArray>
+
+/* Named editors will have id=0, untitled from 1,2... */
+class UntitledCounter {
+    const int RESIZE = 32;
+    QBitArray *bitArray;
+public:
+    UntitledCounter();
+    ~UntitledCounter() {delete bitArray;};
+    int getNextId();
+    bool releaseId(int id);
+};
+
+
+#endif //EDIQ_UNTITLECOUNTER_H
