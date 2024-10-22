722ff336fcfff82c7d66534cc38d1c91c71c36f0
Andrzej Borucki <borucki.andrzej@gmail.com>
2024-10-22 20:39:27 +0200

    methods moved from h to cpp

--- MRU.cpp
+++ MRU.cpp
@@ -1,5 +1,27 @@
-//
-// Created by andrzej on 10/22/24.
-//
-
 #include "MRU.h"
+
+void MRU::add(const QString &item) {
+  m_items.removeAll(item);
+  m_items.prepend(item);
+
+  emit itemAdded(item);
+  if (m_items.size() > m_maxSize) {
+    QString removedItem = m_items.takeLast();
+    emit itemRemoved(removedItem);
+  }
+}
+
+QString MRU::takeItem(const QString &item) {
+  auto index = m_items.indexOf(item);
+  if (index != -1) {
+    QString removedItem = m_items.takeAt(index);
+    emit itemRemoved(removedItem);  // Emituj sygnał usunięcia
+    return removedItem;
+  }
+  return "";
+}
+
+[[nodiscard]] QList<QString> MRU::items() const {
+  return m_items;
+}
+
--- MRU.h
+++ MRU.h
@@ -13,30 +13,9 @@ public:
   explicit MRU(int maxSize, QObject *parent = nullptr)
       : QObject(parent), m_maxSize(maxSize) {}
 
-  void add(const QString &item) {
-    m_items.removeAll(item);
-    m_items.prepend(item);
-
-    emit itemAdded(item);
-    if (m_items.size() > m_maxSize) {
-      QString removedItem = m_items.takeLast();
-      emit itemRemoved(removedItem);
-    }
-  }
-
-  QString takeItem(const QString &item) {
-    int index = m_items.indexOf(item);
-    if (index != -1) {
-      QString removedItem = m_items.takeAt(index);
-      emit itemRemoved(removedItem);  // Emituj sygnał usunięcia
-      return removedItem;
-    }
-    return "";
-  }
-
-  [[nodiscard]] QList<QString> items() const {
-    return m_items;
-  }
+  void add(const QString &item);
+  QString takeItem(const QString &item);
+  [[nodiscard]] QList<QString> items() const;
 
 signals:
   void itemAdded(const QString &item);
